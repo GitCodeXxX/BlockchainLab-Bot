fe3a051cfec1e399d74610c79d0e90de594c6e25
GitCodeXxX <paymentservicequickpay@gmail.com>
2025-04-21 19:15:28 +0200

    ton_api.py aktualisieren

--- ton_api.py
+++ ton_api.py
@@ -1,1 +1,13 @@
+import requests
+from config import TON_API_KEY, TON_API_URL
 
+def get_wallet_balance(address):
+    params = {
+        "address": address,
+        "key": TON_API_KEY
+    }
+    response = requests.get(f"{TON_API_URL}/getAddressBalance", params=params)
+    if response.status_code == 200:
+        balance = int(response.json()["result"]) / 1e9
+        return balance
+    return None
