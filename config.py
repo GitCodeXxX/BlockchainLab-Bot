035fd74efc1c1165d8e28c34fad4993ba968b5b5
GitCodeXxX <paymentservicequickpay@gmail.com>
2025-04-21 19:15:02 +0200

    config.py aktualisieren

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TON_API_KEY = os.getenv("TON_API_KEY")
+TON_API_URL = "https://toncenter.com/api/v2"
