07486d775151d7f4a4fbc21ff2508c8d5cc3750d
XPyCodeX <paymentservicequickpay@gmail.com>
2025-06-06 00:56:03 +0200

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("7534137870:AAHckWCsRJEG9zquCbu_VSxhz5czRwlmBek")
-TON_API_KEY = os.getenv("96a333a8ff4b2c9f9d4d7d70419dc566f6a2a2497faa6be6cb40d78e1d716197")
-TON_API_URL = "https://toncenter.com/api/v2"
