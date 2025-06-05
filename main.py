e2691cf5fb7c19bb3e5b09ccbe3136312ba0cc2c
XPyCodeX <paymentservicequickpay@gmail.com>
2025-06-06 00:56:12 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-@dp.message_handler(commands=['app'])
-async def open_webapp(message: types.Message):
-    kb = types.InlineKeyboardMarkup()
-    btn = types.InlineKeyboardButton(
-        text="Öffne BlockchainLab MiniApp",
-        web_app=types.WebAppInfo(url="https://deinname.github.io/deinrepo/webapp/")
-    )
-    kb.add(btn)
-    await message.answer("Hier ist deine MiniApp:", reply_markup=kb)
